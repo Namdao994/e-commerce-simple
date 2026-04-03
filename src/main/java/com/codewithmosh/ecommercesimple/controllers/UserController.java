138779c8431a320d70ccb0cd9604fca8c2fc04dc
Namdao994 <namdao994@gmail.com>
2026-04-03 12:54:44 +0700

    taskid Add: add parameter

--- src/main/java/com/codewithmosh/ecommercesimple/controllers/UserController.java
+++ src/main/java/com/codewithmosh/ecommercesimple/controllers/UserController.java
@@ -1,16 +1,14 @@
 package com.codewithmosh.ecommercesimple.controllers;
 
 import com.codewithmosh.ecommercesimple.dtos.UserDto;
-import com.codewithmosh.ecommercesimple.entities.User;
 import com.codewithmosh.ecommercesimple.mappers.UserMapper;
 import com.codewithmosh.ecommercesimple.repositories.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 
 @RestController
@@ -21,8 +19,11 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUser() {
-        return userRepository.findAll()
+    public Iterable<UserDto> getAllUser(@RequestParam(required = false, defaultValue = "", name = "name") String sortBy) {
+        if(!Set.of("name", "email").contains(sortBy)) {
+            sortBy = "name";
+        }
+        return userRepository.findAll(Sort.by(sortBy))
                 .stream().map(userMapper::toDto).toList();
     }
 
