9b9a51d816fdbb48594c6bc6fcbb60b788dea5eb
Namdao994 <namdao994@gmail.com>
2026-04-03 14:39:54 +0700

    taskid Add: mo ta ngan

--- src/main/java/com/codewithmosh/ecommercesimple/controllers/UserController.java
+++ src/main/java/com/codewithmosh/ecommercesimple/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.ecommercesimple.controllers;
 
+import com.codewithmosh.ecommercesimple.dtos.RegisterUserRequest;
 import com.codewithmosh.ecommercesimple.dtos.UserDto;
 import com.codewithmosh.ecommercesimple.mappers.UserMapper;
 import com.codewithmosh.ecommercesimple.repositories.UserRepository;
@@ -7,6 +8,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Set;
 
@@ -19,7 +21,9 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUser(@RequestParam(required = false, defaultValue = "", name = "name") String sortBy) {
+    public Iterable<UserDto> getAllUser(
+//            @RequestHeader(required = false, name = "x-auth-token") String authToken,
+            @RequestParam(required = false, defaultValue = "", name = "name") String sortBy) {
         if(!Set.of("name", "email").contains(sortBy)) {
             sortBy = "name";
         }
@@ -34,5 +38,16 @@ public class UserController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(userMapper.toDto(user));
+    }
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(
+            UriComponentsBuilder uriBuilder,
+            @RequestBody RegisterUserRequest request) {
+        var user = userMapper.toEntity(request);
+        userRepository.save(user);
+
+        var userDto = userMapper.toDto(user);
+        var uri = uriBuilder.path("/user/${id").buildAndExpand(userDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(userDto);
     }
 }
--- src/main/java/com/codewithmosh/ecommercesimple/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/ecommercesimple/mappers/UserMapper.java
@@ -1,5 +1,6 @@
 package com.codewithmosh.ecommercesimple.mappers;
 
+import com.codewithmosh.ecommercesimple.dtos.RegisterUserRequest;
 import com.codewithmosh.ecommercesimple.dtos.UserDto;
 import com.codewithmosh.ecommercesimple.entities.User;
 import org.mapstruct.Mapper;
@@ -8,4 +9,5 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto toDto(User user);
+    User toEntity(RegisterUserRequest request);
 }
