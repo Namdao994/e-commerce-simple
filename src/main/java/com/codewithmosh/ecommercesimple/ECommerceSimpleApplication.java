ef9c579991fdeca00aa11fc6028d937929534779
Namdao994 <namdao994@gmail.com>
2026-04-03 08:51:38 +0700

    first commit

---
+++ src/main/java/com/codewithmosh/ecommercesimple/ECommerceSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.ecommercesimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithmosh/ecommercesimple/ECommerceSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.ecommercesimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
