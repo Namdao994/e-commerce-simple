dab19f15d670a19e13aaa753b9e71afa0a857ebc
Namdao994 <namdao994@gmail.com>
2026-04-03 10:04:47 +0700

    task-02 Update: Mo ta ngan

--- src/main/java/com/codewithmosh/ecommercesimple/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/ecommercesimple/repositories/UserRepository.java
@@ -1,6 +1,6 @@
 package com.codewithmosh.ecommercesimple.repositories;
 
-import com.codewithmosh.store.entities.User;
+import com.codewithmosh.ecommercesimple.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
