ce893c5295d45ff692682ee46742eb3d884ef648
Namdao994 <namdao994@gmail.com>
2026-04-03 11:57:26 +0700

    taskid Update: mo ta ngan

---
+++ src/main/java/com/codewithmosh/ecommercesimple/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.codewithmosh.ecommercesimple.controllers;
+
+import com.codewithmosh.ecommercesimple.dtos.UserDto;
+import com.codewithmosh.ecommercesimple.entities.User;
+import com.codewithmosh.ecommercesimple.mappers.UserMapper;
+import com.codewithmosh.ecommercesimple.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @GetMapping
+    public Iterable<UserDto> getAllUser() {
+        return userRepository.findAll()
+                .stream().map(userMapper::toDto).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable  Long id) {
+        var user =  userRepository.findById(id).orElse(null);
+        if(user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userMapper.toDto(user));
+    }
+}
--- src/main/java/com/codewithmosh/ecommercesimple/repositories/UserRepository.java
+++ src/main/java/com/codewithmosh/ecommercesimple/repositories/UserRepository.java
@@ -1,7 +1,8 @@
 package com.codewithmosh.ecommercesimple.repositories;
 
 import com.codewithmosh.ecommercesimple.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
